4822245acbadb07b02460693fc4f207755a3f8b5
xiaoweihua <xiaoweihua@cnhnkj.com>
2021-05-10 22:16:30 +0800

    支持手写@

--- AtDemo/SysTextViewVC/SysTextViewVC.m
+++ AtDemo/SysTextViewVC/SysTextViewVC.m
@@ -108,6 +108,7 @@
     };
 }
 
+#define kATFormat  @"@%@ "
 - (void)updateUIWithUser:(User *)user {
     NSInteger index = self.textView.text.length;
     if (self.textView.isFirstResponder)
@@ -116,22 +117,21 @@
         [self.textView resignFirstResponder];
     }
 
-    NSString *insertText = [NSString stringWithFormat:@"@%@ ", user.name];
+//    NSString *insertText = [NSString stringWithFormat:@"@%@ ", user.name];
 
-    [self.textView insertText:insertText];
-    NSMutableAttributedString *tmpAString = [[NSMutableAttributedString alloc] initWithAttributedString:self.textView.attributedText];
-    [tmpAString setAttributes: @{NSForegroundColorAttributeName:k_hightColor,
-                                 NSFontAttributeName:k_defaultFont}
-                        range:NSMakeRange(self.textView.selectedRange.location - insertText.length, insertText.length)];
-    // 解决光标在插入‘特殊文本’后 移动到文本最后的问题
-    self.textView.attributedText = tmpAString;
-    self.textView.selectedRange = NSMakeRange(index + insertText.length, 0);
+    UITextView *textView = self.textView;
+    NSString *insertString = [NSString stringWithFormat:kATFormat,user.name];
 
-    self.cursorLocation = self.textView.selectedRange.location;
+    NSMutableString *string = [NSMutableString stringWithString:textView.text];
+    [string insertString:insertString atIndex:index];
+    self.textView.text = string;
+    [self textViewDidChange:self.textView];
     
     [self.textView becomeFirstResponder];
+    textView.selectedRange = NSMakeRange(index + insertString.length, 0);
 }
 
+#pragma mark 点击完成
 - (void)done {
     [self.view endEditing:YES];
     
@@ -174,132 +174,8 @@
 }
 
 #pragma mark - UITextViewDelegate
-- (void)textViewDidChangeSelection:(UITextView *)growingTextView
-{
-    // 光标不能点落在@词中间
-    NSRange range = growingTextView.selectedRange;
-    if (range.length > 0)
-    {
-        // 选择文本时可以
-        return;
-    }
-    
-    NSArray *matches = [self findAllAt];
-    
-    for (NSTextCheckingResult *match in matches)
-    {
-        NSRange newRange = NSMakeRange(match.range.location + 1, match.range.length - 1);
-        if (NSLocationInRange(range.location, newRange))
-        {
-            growingTextView.selectedRange = NSMakeRange(match.range.location + match.range.length, 0);
-            break;
-        }
-    }
-}
-
-//- (void)textViewDidChangeSelection:(UITextView *)textView {
-//    // 光标不能点落在@词中间
-//    NSRange range = textView.selectedRange;
-//    if (range.length > 0)
-//    {
-//        // 选择文本时可以
-//        return;
-//    }
-//
-//    NSArray *results = [self findAllAt];
-//
-//    BOOL inRange = NO;
-//    NSRange tempRange = NSMakeRange(0, 0);
-//    NSInteger textSelectedLocation = textView.selectedRange.location;
-//    NSInteger textSelectedLength = textView.selectedRange.length;
-//
-//    for (NSInteger i = 0; i < results.count; i++) {
-//        NSTextCheckingResult *match = results[i];
-////        NSRange range = model.range;
-//        NSRange range = match.range;
-//        if (textSelectedLength == 0) {
-//            if (textSelectedLocation > range.location
-//                && textSelectedLocation < range.location + range.length) {
-//                inRange = YES;
-//                tempRange = range;
-//                break;
-//            }
-//        } else {
-//            if ((textSelectedLocation > range.location && textSelectedLocation < range.location + range.length)
-//                || (textSelectedLocation+textSelectedLength > range.location && textSelectedLocation+textSelectedLength < range.location + range.length)) {
-//                inRange = YES;
-//                break;
-//            }
-//        }
-//    }
-//
-//    if (inRange) {
-//        // 解决光标在‘特殊文本’左右时 无法左右移动的问题
-//        NSInteger location = tempRange.location;
-//        if (self.cursorLocation < textSelectedLocation) {
-//            location = tempRange.location+tempRange.length;
-//        }
-//        textView.selectedRange = NSMakeRange(location, textSelectedLength);
-//        if (textSelectedLength) { // 解决光标在‘特殊文本’内时，文本选中问题
-//            textView.selectedRange = NSMakeRange(textSelectedLocation, 0);
-//        }
-//    }
-//    self.cursorLocation = textView.selectedRange.location;
-//}
-
-- (void)textViewDidChange:(UITextView *)growingTextView
-{
-    UITextRange *selectedRange = growingTextView.markedTextRange;
-    NSString *newText = [growingTextView textInRange:selectedRange];
-
-    if (newText.length < 1)
-    {
-        // 高亮输入框中的@
-        UITextView *textView = self.textView;
-        NSRange range = textView.selectedRange;
-        
-        NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:textView.text];
-        [string addAttribute:NSForegroundColorAttributeName value:[UIColor blackColor] range:NSMakeRange(0, string.string.length)];
-        
-        NSArray *matches = [self findAllAt];
-        
-        for (NSTextCheckingResult *match in matches)
-        {
-            [string addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(match.range.location, match.range.length - 1)];
-        }
-        
-        textView.attributedText = string;
-        textView.selectedRange = range;
-    }
-}
-
-//- (void)textViewDidChange2:(UITextView *)textView {
-//    UITextRange *selectedRange = textView.markedTextRange;
-//    NSString *newText = [textView textInRange:selectedRange];
-//
-//    if (newText.length < 1) {
-//        // 高亮输入框中的@
-//        NSRange range = textView.selectedRange;
-//
-//        NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:textView.text];
-//        [string addAttributes:@{NSForegroundColorAttributeName:k_defaultColor,
-//                               NSFontAttributeName:k_defaultFont}
-//                        range:NSMakeRange(0, string.string.length)];
-//
-//        NSArray *matches = [self findAllAt];
-//
-//        for (NSTextCheckingResult *match in matches) {
-//            [string addAttributes:@{NSForegroundColorAttributeName:k_hightColor,
-//                                   NSFontAttributeName:k_defaultFont}
-//                           range:NSMakeRange(match.range.location, match.range.length - 1)];
-//        }
-//
-//        textView.attributedText = string;
-//        textView.selectedRange = range;
-//    }
-//}
-
-- (BOOL)textView:(UITextView *)growingTextView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+- (BOOL)textView:(UITextView *)growingTextView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+//- (BOOL)growingTextView:(HPGrowingTextView *)growingTextView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
 {
     if ([text isEqualToString:@""])
     {
@@ -331,6 +207,7 @@
         if (inAt)
         {
             growingTextView.text = string;
+            [self textViewDidChange:growingTextView];
             growingTextView.selectedRange = NSMakeRange(index, 0);
             return NO;
         }
@@ -345,54 +222,75 @@
     
     return YES;
 }
+
+//- (void)growingTextViewDidChange:(HPGrowingTextView *)growingTextView
+- (void)textViewDidChange:(UITextView *)growingTextView {
+    
+    UITextRange *selectedRange = growingTextView.markedTextRange;
+    NSString *newText = [growingTextView textInRange:selectedRange];
+
+    if (newText.length < 1)
+    {
+        // 高亮输入框中的@
+        UITextView *textView = self.textView;
+        NSRange range = textView.selectedRange;
+        
+        NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:textView.text];
+        [string addAttribute:NSForegroundColorAttributeName value:[UIColor blackColor] range:NSMakeRange(0, string.string.length)];
+        
+        NSArray *matches = [self findAllAt];
+        
+        for (NSTextCheckingResult *match in matches)
+        {
+            [string addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(match.range.location, match.range.length - 1)];
+        }
+        
+        textView.attributedText = string;
+        textView.selectedRange = range;
+    }
 }
-//- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
-//    // 解决UITextView富文本编辑会连续的问题，且预输入颜色不变的问题
-////    if (textView.textStorage.length != 0) {
-////        textView.typingAttributes = @{NSFontAttributeName:k_defaultFont,NSForegroundColorAttributeName:k_defaultColor};
-////    }
-//
-//    if ([text isEqualToString:@""])
-//    {
-//        NSRange selectRange = textView.selectedRange;
-//        if (selectRange.length > 0)
-//        {
-//            //用户长按选择文本时不处理
-//            return YES;
-//        }
-//
-//        // 判断删除的是一个@中间的字符就整体删除
-//        NSMutableString *string = [NSMutableString stringWithString:textView.text];
-//        NSArray *matches = [self findAllAt];
-//
-//        BOOL inAt = NO;
-//        NSInteger index = range.location;
-//        for (NSTextCheckingResult *match in matches) {
-//            NSRange newRange = NSMakeRange(match.range.location + 1, match.range.length - 1);
-//            if (NSLocationInRange(range.location, newRange)) {
-//                inAt = YES;
-//                index = match.range.location;
-//                [string replaceCharactersInRange:match.range withString:@""];
-//                break;
-//            }
-//        }
-//
-//        if (inAt) {
-//            textView.text = string;
-//            textView.selectedRange = NSMakeRange(index, 0);
-//            return NO;
-//        }
-//    }
-//
-//    //判断是回车键就发送出去
-//    if ([text isEqualToString:@"\n"])
-//    {
-//        [self done];
-//        return NO;
-//    }
-//
-//    return YES;
-//}
+    
+- (void)textViewDidChangeSelection:(UITextView *)growingTextView {
+//- (void)growingTextViewDidChangeSelection:(HPGrowingTextView *)growingTextView
+    // 光标不能点落在@词中间
+    NSRange range = growingTextView.selectedRange;
+    if (range.length > 0)
+    {
+        // 选择文本时可以
+        return;
+    }
+    
+    NSArray *matches = [self findAllAt];
+    
+    for (NSTextCheckingResult *match in matches)
+    {
+        NSRange newRange = NSMakeRange(match.range.location + 1, match.range.length - 1);
+        if (NSLocationInRange(range.location, newRange))
+        {
+            growingTextView.selectedRange = NSMakeRange(match.range.location + match.range.length, 0);
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #pragma mark HNWKeyboardMonitorDelegate
 - (void)keyboardMonitor:(HNWKeyboardMonitor *)keyboardMonitor keyboardWillShow:(HNWKeyboardInfo *)info {
